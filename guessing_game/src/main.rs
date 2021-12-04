d938cf48ebd32183d67cf5b8157af0b542b7c509
youkaiantena <apple.kitaro@gmail.com>
2021-12-05 01:10:56 +0900

    秘密の数字を1 - 100の間で生成できるようにした
    ssh-add -K /path/to/rsaを行わないと、GitHubからrandクレートをダウンロードできなかった

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,15 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
 
     let mut guess = String::new();
+
+    println!("Please input your guess.");
 
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
