de419bb16caf3e12cf1bdeb977249c4bf6ad606b
youkaiantena <apple.kitaro@gmail.com>
2021-12-06 00:00:39 +0900

    秘密の数字が表示されないようにして完成

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,7 +6,6 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number);
 
     loop {
         println!("Please input your guess.");
